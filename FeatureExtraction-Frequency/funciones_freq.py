7326036e3eb3e6a428ad195134bd5c0cb9afd733
Ariel Mardones <vmard001@gmail.com>
2019-11-29 14:56:44 -0300

    Comienzo de WTT + RNN con Keras (13%)

--- FeatureExtraction-Frequency/funciones_freq.py
+++ FeatureExtraction-Frequency/funciones_freq.py
@@ -1,13 +1,10 @@
 import numpy as np
 import pywt
-import MySQLdb
 import scipy
 import scipy.optimize
 import scipy.stats
 import collections
 import datetime
-import params
-from scipy.optimize import least_squares
 import matplotlib
 import pandas as pd
 
