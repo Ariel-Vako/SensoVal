fd98cc1abe9b367d1ebdc5b51121af346ca22e9d
Ariel Mardones <vmard001@gmail.com>
2020-01-31 16:32:54 -0300

    1.- Extracción de datos de cierre (100%) 1.5.- Escalograma (10%) 2.- Transformación de las señales x, y, z en frecuencia sobre bandas de frecuencia (0%) 3.- Caracterización de las señales en frecuencia por banda y eje (0%) 4.- Guardado en base de datos (0%)

--- Enero2020/reconocimiento_fallas.py
+++ Enero2020/reconocimiento_fallas.py
@@ -1,9 +1,19 @@
 import pickle
 from pandas.plotting import register_matplotlib_converters
-
+import scaleogram as scg
+import numpy as np
 from Enero2020 import mysqlQuery_rf
 
 register_matplotlib_converters()
+
+
+def grafica_freq():
+    escala = np.logspace(1.0, 4.0, num=200, dtype=np.int32)
+    ax=scg.cws(year, data, scales, figsize=(12,6), ylabel="Period [Years]", xlabel='Tiempo [s]', yscale='log')
+    ticks = ax.set_yticks([2, 4, 8, 16, 32])
+    ticks = ax.set_yticklabels([2, 4, 8, 16, 32])
+    return
+
 
 valvula = 6  # VALVULA
 ruta = f'/home/arielmardones/Documentos/Respaldo-Ariel/SensoVal/Datos/val{valvula}/'
@@ -17,11 +27,12 @@ with open(file_fecha_apertura, 'rb') as rf1:
 with open(file_fecha_cierre, 'rb') as rf2:
     df_fechas_cierre = pickle.load(rf2)
 
-minutos_antes =5
+minutos_antes = 5
 for cierre in df_fechas_cierre:
     # Extracción de datos antes del cierre
     df = mysqlQuery_rf.query_mysql(cierre[0], minutos_antes)
     # Transformación de las señales x, y, z en frecuencia sobre bandas de frecuencia
+
     # Caracterización de las señales en frecuencia por banda y eje
     # Guardado en base de datos
 
