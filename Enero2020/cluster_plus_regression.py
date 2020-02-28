4e304e1d1c6a4090bbe65b9e67c702bfe2a92ae7
Ariel Mardones <vmard001@gmail.com>
2020-02-28 17:35:45 -0300

    1.- Extracción de datos de cierre (100%) 1.1.- Emparejamiento de fechas entre Mysql y Influx (100%) 1.5.- Escalograma (100%) 2.- Transformación de las señales x, y, z en frecuencia sobre bandas de frecuencia (100%) 3.- Caracterización de las señales en frecuencia por banda y eje (100%) 4.- Guardado en base de datos (100%) 4.1.- Correr script para válvula 8 (100%). 4.2.- Correr script para válvula 9 (100%). 4.3.- Configuración de dataframes para alimentar clustering. (100%) 5.- Clustering (75%)

--- Enero2020/cluster_plus_regression.py
+++ Enero2020/cluster_plus_regression.py
@@ -1,35 +1,57 @@
 # -- coding: utf-8 --
 
 import pickle
+from Enero2020 import clustering
 
-valvulas = [6, 8, 9]
+# # Preformateo previo. Se prepara el dataframe para alimentar a los algoritmos de clustering
 
-# Lectura características válvula 6
-ruta = f'/home/arielmardones/Documentos/Respaldo-Ariel/SensoVal/Datos/val{valvulas[0]}/'
-file6 = ruta + f'Precierre_val{valvulas[0]}'
+# # El pre-formateo se ejecuta solamente la primera vez. Luego guardamos un único dataframe con los resultados.
+# valvulas = [6, 8, 9]
+#
+# # Lectura características válvula 6
+# ruta = f'/home/arielmardones/Documentos/Respaldo-Ariel/SensoVal/Datos/val{valvulas[0]}/'
+# file6 = ruta + f'Precierre_val{valvulas[0]}'
+#
+# with open(file6, 'rb') as rf:
+#     df6 = pickle.load(rf)
+#
+# # Lectura características válvula 8
+# ruta = f'/home/arielmardones/Documentos/Respaldo-Ariel/SensoVal/Datos/val{valvulas[1]}/'
+# file8 = ruta + f'Precierre_val{valvulas[1]}'
+#
+# with open(file8, 'rb') as rf:
+#     df8 = pickle.load(rf)
+#
+# # Lectura características válvula 9
+# ruta = f'/home/arielmardones/Documentos/Respaldo-Ariel/SensoVal/Datos/val{valvulas[2]}/'
+# file9 = ruta + f'Precierre_val{valvulas[2]}'
+#
+# with open(file9, 'rb') as rf:
+#     df9 = pickle.load(rf)
+#
+# features_names = []
+# for eje in ['x', 'y', 'z']:
+#     for wavelet in ['db1', 'db6', 'db8', 'db10']:
+#         for coeficiente in ['cA', 'cD9', 'cD8', 'cD7', 'cD6', 'cD5', 'cD4', 'cD3', 'cD2', 'cD1', 'cD0']:
+#             for features in ['Entropy', 'n° x Cero', 'n° x Media', 'n5', 'n25', 'n75', 'n95', 'median', 'mean', 'std', 'maximo', 'minimo', 'kurtosis', 'skewness']:
+#                 features_names += [features + '-' + coeficiente + '-' + wavelet + '-' + eje]
+#
+# df = df6.append(df8.append(df9))
+# aux = features_names + ['valvula']
+# df.columns = aux
+#
+# ruta2 = f'/home/arielmardones/Documentos/Respaldo-Ariel/SensoVal/Datos/'
+# precierre = ruta2 + f'Precierre'
+# with open(precierre, 'wb') as fl:
+#     pickle.dump(df, fl)
 
-with open(file6, 'rb') as rf:
-    df6 = pickle.load(rf)
+ruta = f'/home/arielmardones/Documentos/Respaldo-Ariel/SensoVal/Datos/'
+file = ruta + f'Precierre'
 
-# Lectura características válvula 8
-ruta = f'/home/arielmardones/Documentos/Respaldo-Ariel/SensoVal/Datos/val{valvulas[1]}/'
-file8 = ruta + f'Precierre_val{valvulas[1]}'
+with open(file, 'rb') as rf:
+    df = pickle.load(rf)
 
-with open(file8, 'rb') as rf:
-    df8 = pickle.load(rf)
 
-# Lectura características válvula 9
-ruta = f'/home/arielmardones/Documentos/Respaldo-Ariel/SensoVal/Datos/val{valvulas[2]}/'
-file9 = ruta + f'Precierre_val{valvulas[2]}'
-
-with open(file9, 'rb') as rf:
-    df9 = pickle.load(rf)
-
-features_names = []
-for eje in ['x', 'y', 'z']:
-    for wavelet in ['db1', 'db6', 'db8', 'db10']:
-        for coeficiente in ['cA', 'cD9', 'cD8', 'cD7', 'cD6', 'cD5', 'cD4', 'cD3', 'cD2', 'cD1', 'cD0']:
-            for features in ['Entropy', 'n° x Cero', 'n° x Media', 'n5', 'n25', 'n75', 'n95', 'median', 'mean', 'std', 'maximo', 'minimo', 'kurtosis', 'skewness']:
-                features_names += [eje]
+medidas = clustering.métricas(df,7)
 
 print('')
--- Enero2020/clustering.py
+++ Enero2020/clustering.py
@@ -9,53 +9,9 @@ from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 import sklearn.metrics
 from sklearn.preprocessing import StandardScaler
-import params
 
 
-def clustering_open(signal_features, no_cluster=7):
-    # kmeans = KMeans(n_clusters=no_cluster, random_state=0).fit(signal_features)
-    # mini_kmeans = MiniBatchKMeans(n_clusters=no_cluster, random_state=0, max_iter=10).fit(signal_features)
-    # af = AffinityPropagation(preference=-10).fit(signal_features)
-    #
-    # # --- Mean Shift
-    # bandwidth = estimate_bandwidth(signal_features, quantile=0.8)
-    # ms = MeanShift(bandwidth=bandwidth, bin_seeding=True)
-    # ms.fit(signal_features)
-    # # ms.labels_.max()
-    #
-    # # --- Spectral Clustering
-    # affinity = np.exp(-euclidean_distances(signal_features) / np.std(signal_features))
-    # labels_sc = spectral_clustering(affinity, n_clusters=no_cluster, eigen_solver='arpack')
-    #
-    # # --- Agglomerative Clustering - Solo métrica euclidiana
-    # clustering_ward = AgglomerativeClustering(linkage='ward', n_clusters=no_cluster)
-    # clustering_ward.fit(signal_features)
-    # clustering_average = AgglomerativeClustering(linkage='average', n_clusters=no_cluster)
-    # clustering_average.fit(signal_features)
-    # clustering_complete = AgglomerativeClustering(linkage='complete', n_clusters=no_cluster)
-    # clustering_complete.fit(signal_features)
-    # clustering_single = AgglomerativeClustering(linkage='single', n_clusters=no_cluster)
-    # clustering_single.fit(signal_features)
-    #
-    # # --- DBSCAN
-    # db = DBSCAN(eps=8, min_samples=6).fit(signal_features)
-    #
-    # # --- OPTICS
-    # optics = OPTICS(min_samples=6, xi=.05, min_cluster_size=.1)
-    # optics.fit(signal_features)
-    #
-    # # --- Birch
-    # brc = Birch(branching_factor=100, n_clusters=no_cluster, threshold=20, compute_labels=True)
-    # brc.fit(signal_features)
-
-    # --- Agglomerative Clustering - Solo métrica euclidiana
-    clustering_complete = AgglomerativeClustering(linkage='complete', n_clusters=no_cluster)
-    clustering_complete.fit(signal_features)
-
-    return clustering_complete
-
-
-def clustering_close(signal_features, no_cluster=7):
+def clustering_precierre(signal_features, no_cluster=7):
     # kmeans = KMeans(n_clusters=no_cluster, random_state=0).fit(signal_features)
     # mini_kmeans = MiniBatchKMeans(n_clusters=no_cluster, random_state=0, max_iter=10).fit(signal_features)
     # af = AffinityPropagation(preference=-10).fit(signal_features)
